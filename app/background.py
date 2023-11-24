13531e4a105fb3199e656b2b6c48deb726428ee2
Gabben <43146729+gabbhack@users.noreply.github.com>
2023-11-24 20:40:29 +0300

    Loguru for logging

--- app/background.py
+++ app/background.py
@@ -1,6 +1,3 @@
-import os
-import tempfile
-import traceback
 from pathlib import Path
 from typing import Any
 from uuid import UUID
@@ -8,6 +5,7 @@ from uuid import UUID
 import librosa
 import torch
 from arq.connections import RedisSettings
+from loguru import logger
 from transformers import (
     AutoModelForSpeechSeq2Seq,
     AutoProcessor,
@@ -47,59 +45,68 @@ pipe = pipeline(
 
 
 async def analyze(_ctx: dict[str, Any], lecture_id: UUID) -> None:
+    context_logger = logger.bind(lecture_id=lecture_id)
+    context_logger.info("Start")
+
     lecture = await get_lecture(edgedb.client, id=lecture_id)
 
-    print(lecture)
-    print(lecture is None)
     if lecture is None:
+        context_logger.warning("No such lecture in database")
         return
 
-    print(lecture.object_name is None)
-    if lecture.object_name is None:
+    if lecture.filename is None or lecture.object_name is None:
+        context_logger.warning("Lecture doesn't have file")
         await finish_analysis(
             edgedb.client,
             lecture_id=lecture_id,
             status="Error",
             text=None,
-            error="Lecture without file"
+            error="Lecture without file",
         )
         return
 
     path = lecture.object_name + lecture.filename
-    print(path)
     try:
-        print("Starting downloading")
+        context_logger.info("Downloading file")
         await minio.client.fget_object(
             bucket_name=SETTINGS.s3_bucket,
             object_name=lecture.object_name,
-            file_path=path
+            file_path=path,
         )
-        print("Downloaded, librosa")
+        context_logger.info("Librosa loading")
         audio = librosa.load(path, sr=16_000)[0]
-        print("Starting pipe")
+        context_logger.info("First model processing")
         result = pipe(audio, generate_kwargs={"language": "russian"})
-        print(result)
+        context_logger.info("First model processing complete")
+
         await finish_analysis(
             edgedb.client,
             lecture_id=lecture_id,
             status="Processed",
             text=result["text"],
-            error=None
+            error=None,
         )
     except Exception as error:
-        print("Error!", repr(error), error.__class__)
-        traceback.print_exc()
-        await finish_analysis(edgedb.client, lecture_id=lecture_id, status="Error", text=None, error=repr(error))
+        context_logger.exception("Error")
+        await finish_analysis(
+            edgedb.client,
+            lecture_id=lecture_id,
+            status="Error",
+            text=None,
+            error=repr(error),
+        )
     finally:
         Path(path).unlink(missing_ok=True)
 
 
 async def startup(_ctx: dict[str, Any]) -> None:
     await edgedb.client.ensure_connected()
-    await edgedb.client.execute("""
+    await edgedb.client.execute(
+        """
         CONFIGURE INSTANCE SET session_idle_timeout :=
             <duration>'5 minutes';
-    """)
+    """
+    )
     await edgedb.client.execute(
         """
         CONFIGURE INSTANCE SET session_idle_transaction_timeout :=
