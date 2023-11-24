ea4dd38b9caeedd8b83bf72d09221b7974604fd2
Gabben <43146729+gabbhack@users.noreply.github.com>
2023-11-24 09:10:54 +0300

    Удалил значения по умолчанию для настроек

--- app/settings.py
+++ app/settings.py
@@ -4,12 +4,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     edgedb_dsn: AnyUrl | None = None
-    redis_dsn: RedisDsn = "redis://localhost"
-    s3_host: str = "localhost:443"
-    s3_access_key: str = "edCFhCQApXZm0bvRgeQX"
-    s3_secret_key: str = "gqVyjEdL4wss7LBYxS3Qpzrd4mSVagZz5reCqtUw"
-    s3_bucket: str = "bucket"
-    s3_public_host: str = "https://minio.sosus.org"
+    redis_dsn: RedisDsn
+    s3_host: str
+    s3_access_key: str
+    s3_secret_key: str
+    s3_bucket: str
+    s3_public_host: str
 
 
 SETTINGS = Settings()
